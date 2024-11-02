74b985baad16d0035a3a5bb619004c4a396e9245
rihansalmanii <salmanirihan76@gmail.com>
2024-11-02 17:56:13 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let compScore = 0;
+ const choices = document.querySelectorAll(".choice");
+
+ choices.forEach((choice) => {
+    choice.addEventListener("click", () =>{
+        console.log("box was clicked");
+        const choiceId = choice.getAttribute("id");
+        console.log(choice, choiceId);
+    })
+ })
